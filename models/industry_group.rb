e0c11a68c95618057186e4446802f147a532b192
jmalochee <jmalochee@gmail.com>
2017-06-28 16:44:57 -0400

    industry_group model

--- models/industry_group.rb
+++ models/industry_group.rb
@@ -1,2 +1,7 @@
 class IndustryGroup < ActiveRecord::Base
+  validates :code, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true
+
+  belongs_to :major_group
+  has_many :industries
 end
