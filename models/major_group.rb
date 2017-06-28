1936a60d2faaa582832b720fe6b7b62cd077598b
jmalochee <jmalochee@gmail.com>
2017-06-28 16:46:29 -0400

    major_group model

--- models/major_group.rb
+++ models/major_group.rb
@@ -1,2 +1,8 @@
 class MajorGroup < ActiveRecord::Base
+  validates :code, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true
+  validates :definition, presence: true
+
+  belongs_to :division
+  has_many :industries
 end
