8f8355e08bfacee893f346f6857e87598c1e8302
jmalochee <jmalochee@gmail.com>
2017-06-28 16:43:35 -0400

    industry model

--- models/industry.rb
+++ models/industry.rb
@@ -1,10 +1,10 @@
 class Industry < ActiveRecord::Base
-  validates :industry_code, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :industry_name, presence: true
+  validates :code, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true
+  validates :definition, presence: true
   validates :division_code, presence: true, length: { is: 1 }, format: { with: /[A-J]/, message: "only A-J valid" }
   validates :major_group_code, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :industry_group_code, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :industry_definition, presence: true
 
   belongs_to :industry_group
   belongs_to :major_group
